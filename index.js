b4aa8a90d428a9811d53545bb0e4e84b64ee0ff8
Wilson <wilsonj806@gmail.com>
2022-01-11 19:26:18 -0500

    add process env port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const res = require('express/lib/response')
 require('dotenv').config()
 
 const app = require('express')()
-const port = 3000
+const port = process.env.PORT || 3000
 app.use(bodyParser.json())
 app.use(compression())
 app.use(helmet())
